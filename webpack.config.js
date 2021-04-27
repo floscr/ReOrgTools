02324929440ed9e580ea1b5948a0dd63480e3b2d
Florian Schroedl <flo.schroedl@gmail.com>
2021-04-27 20:35:09 +0200

    Seperate configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,35 @@
 const path = require('path');
 const isProduction = process.env.NODE_ENV === 'production';
 
-const config = isProduction ? require('./webpack.config.production.js') : {};
-
-module.exports = {
-  // ...config,
-  stats: 'errors-only',
-  entry: './src/WebService/Index.bs.js',
-
-  mode: isProduction ? 'production' : 'development',
-
-  output: {
-    path: path.join(__dirname, "bundleOutput"),
-    publicPath: '/',
-    filename: 'index.js',
-  },
+const devConfig = {
+  mode: 'development',
   devServer: {
     port: 8080,
     hot: true,
     inline: true,
     historyApiFallback: true,
   },
+}
+
+const prodConfig = {
+  mode: 'productions',
+}
+
+const extendConfig = isProduction ? devConfig : prodConfig;
+
+module.exports = {
+  ...extendConfig,
+
+  stats: 'errors-only',
+
+  entry: './src/WebService/Index.bs.js',
+
+  output: {
+    path: path.join(__dirname, "bundleOutput"),
+    publicPath: '/',
+    filename: 'index.js',
+  },
+
   module: {
     rules: [
       {
