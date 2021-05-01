fe085854b16dbe53fb1817c29f3e3fe573b2dee1
Florian Schroedl <flo.schroedl@gmail.com>
2021-05-01 14:09:57 +0200

    Cleanup

--- esbuild.js
+++ esbuild.js
@@ -4,26 +4,16 @@ const esbuild = require('esbuild')
 const ssri = require('ssri')
 const ejs = require('ejs')
 
-const isUnixHiddenPath = function (path) {
-    return (/(\/|^)\.[^\/\.]/g).test(path);
-};
-
 // Config
+
 const srcDir = './src/WebService';
 const distDir = './src/Server/public/dist';
-
 const entryPoints = ['Index.bs.js'].map(x => path.join(srcDir, x))
 const outFile = path.join(distDir, 'index.js');
 
-const clean = () => {
-    const files = fs
-        .readdirSync(distDir)
-        .filter(x => !isUnixHiddenPath(x))
-        .map(x => path.join(distDir, x))
-        .forEach(x => fs.unlinkSync(x))
-}
+// Plugins
 
-const makeHtml = async () => {
+const makeHtml = () => {
     const replacements = {
         jsSrc: '/dist/index.js',
         jsIntegrity: ssri.fromData(
@@ -38,6 +28,8 @@ const makeHtml = async () => {
     })
 }
 
+// Main
+
 const build = async () => {
     await esbuild.build({
         entryPoints,
@@ -50,5 +42,4 @@ const build = async () => {
 
     makeHtml();
 }
-
 build();
