aa8993cbd6059d201a1f60f8bd409f78da23e837
Florian Schroedl <flo.schroedl@gmail.com>
2021-05-01 14:08:37 +0200

    Add hash to template

--- esbuild.js
+++ esbuild.js
@@ -1,18 +1,54 @@
-#!/usr/bin/env node
+const path = require('path')
+const fs = require('fs')
+const esbuild = require('esbuild')
+const ssri = require('ssri')
+const ejs = require('ejs')
 
-let esbuild = require('esbuild')
+const isUnixHiddenPath = function (path) {
+    return (/(\/|^)\.[^\/\.]/g).test(path);
+};
+
+// Config
+const srcDir = './src/WebService';
+const distDir = './src/Server/public/dist';
+
+const entryPoints = ['Index.bs.js'].map(x => path.join(srcDir, x))
+const outFile = path.join(distDir, 'index.js');
+
+const clean = () => {
+    const files = fs
+        .readdirSync(distDir)
+        .filter(x => !isUnixHiddenPath(x))
+        .map(x => path.join(distDir, x))
+        .forEach(x => fs.unlinkSync(x))
+}
+
+const makeHtml = async () => {
+    const replacements = {
+        jsSrc: '/dist/index.js',
+        jsIntegrity: ssri.fromData(
+            fs.readFileSync(outFile),
+            { algorithms: ['sha256'] }
+        ),
+    };
+
+    ejs.renderFile(path.join(srcDir, 'index.ejs'), replacements, {}, (err, str) => {
+        if (err) console.error(err)
+        fs.writeFileSync(path.join(distDir, '../index.html'), str)
+    })
+}
 
 const build = async () => {
     await esbuild.build({
-        entryPoints: ['./src/WebService/Index.bs.js' ],
+        entryPoints,
         logLevel: 'info',
         bundle: true,
         minify: true,
         sourcemap: 'external',
-        outfile: './src/Server/public/dist/index.js'
+        outfile: outFile,
     })
 
-    console.log('done');
+    makeHtml();
 }
 
 build();
